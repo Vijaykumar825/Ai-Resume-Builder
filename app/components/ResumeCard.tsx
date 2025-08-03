6804be9e7c832d4a451dd50d6f2548b0508f57b7
vijay <kumarvijay34453@gmail.com>
2025-08-03 09:37:03 +0530

    loading the real resume which uploaded by you

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,6 +1,21 @@
 import { Link } from "react-router"; // <-- Correct
 import ScoreCircle from "./ScoreCircle";
+import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
 const ResumeCard = ({ resume:{id,companyName,jobTitle,feedback,imagePath} }: { resume: Resume }) => {
+  const { fs} = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState('');
+  useEffect(() => {
+      const loadResumes = async () => {
+        const blob = await fs.read(imagePath);
+        if (!blob) return;
+        let Url= URL.createObjectURL(blob);
+        setResumeUrl(Url);
+      }
+      loadResumes();
+    }, [imagePath]);
+
+  
   return (
     <Link
       to={`/resumes/${id}`}
@@ -8,12 +23,13 @@ const ResumeCard = ({ resume:{id,companyName,jobTitle,feedback,imagePath} }: { r
     >
       <div className="resume-card-header">
         <div className="flex flex-col gap-2">
-          <h2 className="text-black font-bold break-words">
+          {companyName && <h2 className="text-black font-bold break-words">
             {companyName}
-          </h2>
-          <h3 className="text-lg break-words text-grey-500">
+          </h2>}
+          {jobTitle && <h3 className="text-lg break-words text-grey-500">
             {jobTitle}
-          </h3>
+          </h3>}
+          {!companyName && !jobTitle && <h2 className="!text-black font-black">Resume</h2>}
 
         </div>
         <div className="flex-shrink">
@@ -22,15 +38,17 @@ const ResumeCard = ({ resume:{id,companyName,jobTitle,feedback,imagePath} }: { r
       </div>
       
       
-      <div className="gradient-border animate-in fade-in duration-1000">
+      {resumeUrl && 
+      (<div className="gradient-border animate-in fade-in duration-1000">
         <div className="w-full h-full ">
           <img
-            src={imagePath}
+            src={resumeUrl}
             alt="resume"
             className="w-full h-[350px] max-sm:h-[200] object-cover object-top"
           />
         </div>
-      </div>
+      </div>)
+      }
       
     </Link>
   );
