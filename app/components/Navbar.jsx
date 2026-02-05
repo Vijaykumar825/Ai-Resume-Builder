0780a5980eec41d54a194cfec1a648e389463d11
Addakula Vijay Kumar <kumarvijay34453@gmail.com>
2026-02-05 11:02:58 +0530

    Fix: Navbar Link import and added Logout feature

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router'
+import { useNavigate, Link } from 'react-router';
+import { usePuterStore } from '~/lib/puter';
 
 const Navbar = () => {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    navigate('/');
+  };
+
   return (
     <nav className="navbar">
       <Link to="/">
         <p className="text-2xl font-bold text-gradient">RESUMIND</p>
       </Link>
-      <Link to="/upload" className="primary-button w-fit">
-        Upload Resume
-      </Link>
-      
+      <div className="flex gap-4 items-center">
+        <Link to="/upload" className="primary-button w-fit">
+          Upload Resume
+        </Link>
+        {auth.isAuthenticated && (
+          <button onClick={handleLogout} className="text-red-500 font-semibold hover:text-red-600 transition-colors cursor-pointer">
+            Logout
+          </button>
+        )}
+      </div>
     </nav>
   )
 }
