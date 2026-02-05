792dd31856c0d68c323c3d95e611b4027c8f4a70
Addakula Vijay Kumar <kumarvijay34453@gmail.com>
2026-02-05 11:10:58 +0530

    done

--- app/routes/Auth.jsx
+++ app/routes/Auth.jsx
@@ -11,13 +11,14 @@ export const meta=() => ([
 const Auth = () => {
   const {isLoading,auth} = usePuterStore();
   const location=useLocation()
-  const next=location.search.split('next=')[1];
+  const next = location.search.split('next=')[1] || '/';
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (auth.isAuthenticated) navigate(next);
-      
-  }, [auth.isAuthenticated,next]);
+    if (auth.isAuthenticated) {
+      navigate(next);
+    }
+  }, [auth.isAuthenticated, next]);
   return (
     <main className='bg-[url("/images/bg-auth.svg")] bg-cover h-screen flex items-center justify-center'>
       <div className='gradent-border shadow-lg'>
